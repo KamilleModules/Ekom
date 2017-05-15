581e659f6c4d4e8824e83c2c6c3e530e5ceafd59
lingtalfi <lingtalfi@gmail.com>
2017-05-15 17:30:10 +0200

    update

--- Utils/E.php
+++ Utils/E.php
@@ -9,9 +9,9 @@ use Kamille\Utils\Routsy\LinkGenerator\ApplicationLinkGenerator;
 class E
 {
 
-    public static function link($route, array $params = [])
+    public static function link($routeId, array $params = [])
     {
-        return ApplicationLinkGenerator::getUri($route, $params);
+        return ApplicationLinkGenerator::getUri($routeId, $params);
     }
 
 }
