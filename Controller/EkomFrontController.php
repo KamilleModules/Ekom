b8ce580b1be8d4414547bc0802ec5640ce6a3c9e
lingtalfi <lingtalfi@gmail.com>
2017-07-21 08:13:14 +0200

    update

--- Controller/EkomFrontController.php
+++ Controller/EkomFrontController.php
@@ -4,8 +4,10 @@
 namespace Controller\Ekom;
 
 
+use Bat\SessionTool;
 use Bat\UriTool;
 use Core\Controller\ApplicationController;
+use Core\Services\Hooks;
 use Kamille\Architecture\Response\Web\RedirectResponse;
 use Kamille\Utils\Laws\Config\LawsConfig;
 use Module\Ekom\Api\EkomApi;
@@ -15,6 +17,7 @@ use Module\Ekom\Utils\E;
 class EkomFrontController extends ApplicationController
 {
 
+    protected $setSessionReferer = true;
 
 //    protected function renderPageError($msg, $title = null)
 //    {
@@ -37,7 +40,6 @@ class EkomFrontController extends ApplicationController
 
     protected function renderByViewId($viewId, LawsConfig $config = null, array $options = [])
     {
-
         /**
          * From here we can configure all the common/implicit widgets.
          * Those are the widgets specific to ekom module and common to all controllers.
@@ -45,14 +47,27 @@ class EkomFrontController extends ApplicationController
         if (null === $config) {
             $config = LawsConfig::create();
         }
+
+        $commonWidgets = [];
+        Hooks::call("Ekom_FrontController_decorateCommonWidgets", $commonWidgets);
         $config->replace(([
-            "widgets" => [
-                "topActionBar.miniCart" => [
-                    "tpl" => "Ekom/MiniCart/sliding",
-                    "conf" => EkomApi::inst()->cartLayer()->getCartInfo(),
-                ],
-            ],
+            "widgets" => $commonWidgets,
         ]));
+
+
+        if (true === $this->setSessionReferer) {
+            $_SESSION['ekom.referer'] = UriTool::uri(null, [], true, true);
+        }
+
+
+//        $configInfo = [
+//            $viewId,
+//            $config,
+//        ];
+//        Hooks::call("Ekom_decorate_LawsConfig", $configInfo);
+//        $conf = $configInfo[1];
+
+
         return parent::renderByViewId($viewId, $config, $options);
     }
 
@@ -63,7 +78,6 @@ class EkomFrontController extends ApplicationController
         $link = E::link("Ekom_login", [], true);
         return RedirectResponse::create($link);
     }
-
 
 
 }
